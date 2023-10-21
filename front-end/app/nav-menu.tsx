6f43b52dc90b7150e13b00c172a4ef97e877c6ea
Oleksii Popov <alexejpopovua@gmail.com>
2023-10-21 21:11:28 +0200

    chore: header styling

--- front-end/app/nav-menu.tsx
+++ front-end/app/nav-menu.tsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
+import {clsx} from "clsx";
 
-const itemCls = "select-none space-y-1 rounded-md py-3 mr-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground";
+const itemCls = "select-none rounded-md py-3 px-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground";
+const titleItemCls = "select-none rounded-md leading-none no-underline outline-none";
 
 const NavMenu = () => (
-    <nav className="px-4 py-4">
-        <Link href="/" className={itemCls}>
-            Oleksii Popov
-        </Link>
-        <Link href="/blog" className={itemCls}>
-            Blog
-        </Link>
-        <Link href="/portfolio" className={itemCls}>
-            Portfolio
-        </Link>
-        <Link href="/resume" className={itemCls}>
-            CV
-        </Link>
-        <Link href="/contact" className={itemCls}>
-            Contact
-        </Link>
+    <nav className="px-4 py-4 flex flex-row">
+        <div className="flex items-center">
+            <Link href="/" className={clsx(titleItemCls, "text-2xl")}>
+                Oleksii Popov
+            </Link>
+        </div>
+        <div className="flex flex-row grow justify-end text-lg">
+            <Link href="/blog" className={itemCls}>
+                Blog
+            </Link>
+            <Link href="/portfolio" className={itemCls}>
+                Portfolio
+            </Link>
+            <Link href="/resume" className={itemCls}>
+                CV
+            </Link>
+            <Link href="/contact" className={itemCls}>
+                Contact
+            </Link>
+        </div>
     </nav>
 );
 
