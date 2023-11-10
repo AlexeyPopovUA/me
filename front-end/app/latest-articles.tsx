9e2571ceb3727145a31e00c648c565bf636c4647
Oleksii Popov <alexejpopovua@gmail.com>
2023-11-10 21:29:02 +0100

    chore: home page content and layout corrections

--- front-end/app/latest-articles.tsx
+++ front-end/app/latest-articles.tsx
@@ -9,13 +9,7 @@ const LatestArticles = async () => {
             {lastNArticles.map(article => (
                 <Link href={`/blog/${article.slug}`} passHref key={article.slug}>
                     <div className='py-2 flex justify-between align-middle gap-2'>
-                        <div>
-                            <h3 className="text-lg font-bold">{article.title}</h3>
-                            <p className="text-gray-400">{article.description}</p>
-                        </div>
-                        <div className="my-auto text-gray-400">
-                            <p>{article.date}</p>
-                        </div>
+                        <h3 className="text-lg font-bold">{article.title}</h3>
                     </div>
                 </Link>
             ))}
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -5,18 +5,14 @@ import LatestArticles from "@/app/latest-articles";
 export default function Home() {
     return (
         <main className="flex flex-col items-center py-4 px-8">
-            <div className="mb-12 flex place-items-center">
-                <h1 className="text-2xl">Software development blog and portfolio</h1>
-            </div>
             <div className="text-lg mb-24">
                 <section>
                     <p className="mb-4 text-xl">Hi, I`&apos;m Oleksii Popov</p>
                     <p className="mb-4">I develop full stack solutions with React, NodeJS, JavaScript, TypeScript and
                         AWS</p>
-                    <div className="mb-4">You can find my CV <Link href="/cv"
-                                                                   className="font-semibold accent-slate-500">here</Link>
-                    </div>
-                    <p className="mb-4">Sometimes, I write articles. Here are some of them:</p>
+                    <p className="mb-4">During my web development work, I meet and create interesting solutions in front-end, cloud and back-end areas.
+                        I strongly believe that sharing those things brings much more value, than just using silently. So, that will be the main purpose of my blog.</p>
+                    <p className="mb-4">I write articles. Here are some of them:</p>
                 </section>
                 <section className="pl-4">
                     <LatestArticles/>
