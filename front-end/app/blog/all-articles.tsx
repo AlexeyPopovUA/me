d8c1c0401e6349e529be314ca2e1886818b90c6b
Oleksii Popov <alexejpopovua@gmail.com>
2023-12-17 21:28:26 +0100

    feat: render the article date

--- front-end/app/blog/all-articles.tsx
+++ front-end/app/blog/all-articles.tsx
@@ -10,8 +10,9 @@ const AllArticles = async () => {
             {lastNArticles.map(article => (
                 <div className="flex flex-col items-stretch pb-4 border-b-2 border-solid border-gray-200" key={article.slug}>
                     {article.thumbnail ? <div className="flex flex-row"><BlogThumbnailImage src={article.thumbnail} alt={"test"}/></div> : null}
-                    <Link className="hover:text-amber-600 font-bold text-xl underline mb-4" href={`/blog/${article.slug}`} passHref
+                    <Link className="hover:text-amber-600 font-bold text-xl underline mb-2" href={`/blog/${article.slug}`} passHref
                           key={article.slug}>{article.title}</Link>
+                    <div className="text-sm mb-4">{article.date}</div>
                     <div className="text-md text-gray-600">{article.description}</div>
                 </div>
             ))}
