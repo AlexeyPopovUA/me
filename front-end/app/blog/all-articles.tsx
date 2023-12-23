a383efa4e1b1028f7a9a6de922f9670457fe8ed4
Oleksii Popov <alexejpopovua@gmail.com>
2023-12-23 15:35:03 +0100

    chore: blog items styling

--- front-end/app/blog/all-articles.tsx
+++ front-end/app/blog/all-articles.tsx
@@ -8,13 +8,19 @@ const AllArticles = async () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-24">
             {lastNArticles.map(article => (
-                <div className="flex flex-col items-stretch pb-4 border-b-2 border-solid border-gray-200" key={article.slug}>
-                    {article.thumbnail ? <div className="flex flex-row mb-4"><BlogThumbnailImage src={article.thumbnail} alt={"test"}/></div> : null}
-                    <Link className="hover:text-amber-600 font-bold text-xl underline mb-2" href={`/blog/${article.slug}`} passHref
-                          key={article.slug}>{article.title}</Link>
-                    <div className="text-sm mb-4">{article.date}</div>
-                    <div className="text-md text-gray-600">{article.description}</div>
-                </div>
+                <Link className="hover:text-amber-600" href={`/blog/${article.slug}`} passHref
+                      key={article.slug}>
+                    <div
+                        className="flex flex-col items-stretch pb-4 border-b-2 border-solid border-gray-200 hover:text-amber-600"
+                        key={article.slug}>
+                        {article.thumbnail ?
+                            <div className="flex flex-row mb-4"><BlogThumbnailImage src={article.thumbnail}
+                                                                                    alt={"test"}/></div> : null}
+                        <div className="font-bold text-xl underline mb-2">{article.title}</div>
+                        <div className="text-sm text-gray-600 mb-4">{article.date}</div>
+                        <div className="text-md text-gray-600">{article.description}</div>
+                    </div>
+                </Link>
             ))}
         </div>
     );
