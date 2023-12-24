2984e4c05537f29ee8735ce3f236c387bfc8ca00
Oleksii Popov <alexejpopovua@gmail.com>
2023-12-24 21:27:07 +0100

    feat: add simple footer

--- front-end/app/footer.tsx
+++ front-end/app/footer.tsx
@@ -1,72 +1,7 @@
 const Footer = () => (
-    <div className="grid text-center md:grid-cols-4 md:text-left">
-        <a
-            href="/about"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                About{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Who am I?
-            </p>
-        </a>
-
-        <a
-            href="/blog"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                Blog{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Check out my latest articles
-            </p>
-        </a>
-
-        <a
-            href="/portfolio"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                Portfolio{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                My latest projects
-            </p>
-        </a>
-
-        <a
-            href="/contact"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                Contact{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Contact information
-            </p>
-        </a>
+    <div className="flex-col text-center p-8 border-t-2">
+        <div className="mb-4">Developed by Oleksii Popov</div>
+        <div>{new Date().getFullYear()}</div>
     </div>
 );
 
