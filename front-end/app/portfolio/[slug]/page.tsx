6cfcbd55c27d7ad899fc5da54f18203b889a2924
Oleksii Popov <alexejpopovua@gmail.com>
2024-01-21 21:32:05 +0100

    fix: do not stretch portfolio pages with a long content

--- front-end/app/portfolio/[slug]/page.tsx
+++ front-end/app/portfolio/[slug]/page.tsx
@@ -31,15 +31,15 @@ export default async function Post(props: StaticProps) {
     return (
         <article className='prose prose-sm md:prose-base lg:prose-lg prose-pre:bg-white prose-pre:p-0 mx-auto p-4'>
             <h1>{post.title}</h1>
-            <div>{post.technologies}</div>
+            <div className="flex flex-row flex-wrap">{post.technologies.map(item => <span
+                key={item}>{item}</span>)}</div>
             <div>{post.type}</div>
             <div>{post.description}</div>
             <div>{post.company}</div>
             <div>{post.thumbnail}</div>
             <div>{post["main-features"]}</div>
             <div>{post["my-commitment"]}</div>
-            {/*<Experiment slideCfgs={post.gallery.map(imagePath => ({path: imagePath, width: 1000}))} />*/}
-            <Gallery images={post.gallery} />
+            <Gallery images={post.gallery}/>
         </article>
     );
 }
