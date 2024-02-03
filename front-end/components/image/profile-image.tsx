758ba9fb02f20457240bf387e1f369243946ee01
Oleksii Popov <alexejpopovua@gmail.com>
2024-02-03 17:04:50 +0100

    fix: make profile photo file smaller

--- front-end/components/image/profile-image.tsx
+++ front-end/components/image/profile-image.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import {imageLoader} from "@/components/image/image-loader";
 import React from "react";
 import Image from "next/image";
 
+import {imageLoader} from "@/components/image/image-loader";
+
 const ProfileImage = () => <Image className="rounded-full border-4 border-amber-500 aspect-square" loader={imageLoader}
-                            placeholder="blur"
-                            blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-                            width={300} height={300} src="/pages/home/me-w-square-bg.jpg" alt="My profile photo"/>;
+                                  placeholder="blur"
+                                  quality={70}
+                                  blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+                                  width={300} height={300} src="/pages/home/me-w-square-bg.jpg"
+                                  alt="My profile photo"/>;
 
 export default ProfileImage;
