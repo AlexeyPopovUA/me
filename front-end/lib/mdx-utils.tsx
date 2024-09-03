7327c9d92adf1313c6059597c72ee3040ef4a5f9
Oleksii Popov <alexejpopovua@gmail.com>
2024-09-03 22:32:34 +0200

    feat(front-end): add support of article videos with poster to MDX

--- front-end/lib/mdx-utils.tsx
+++ front-end/lib/mdx-utils.tsx
@@ -8,33 +8,44 @@ import rehypeRaw from "rehype-raw";
 import {compileMDX, MDXRemoteProps} from "next-mdx-remote/rsc";
 import {read} from "to-vfile";
 import remarkGfm from 'remark-gfm'
+import React from "react";
+import {getInsideImageURL} from "@/lib/image";
 
 const cmp = {
-    // Note, that MDXImage is a server component, therefore wrapped to match types
-    img: (props: any) => <MDXImage {...props} />,
-    table: (props: any) => <div className="table-wrapper"><table {...props} /></div>
+  // Note, that MDXImage is a server component, therefore wrapped to match types
+  img: (props: any) => <MDXImage {...props} />,
+  table: (props: any) => <div className="table-wrapper">
+    <table {...props} />
+  </div>,
+  ArticleVideo: (props: React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>) => {
+    const poster = props.poster ? getInsideImageURL({src: props.poster, width: 800, height: 800 / 1.736}) : undefined;
+    return <video width="100%" poster={poster} controls preload="none" loop muted>
+      <source src={props.src}/>
+      Your browser does not support the video tag.
+    </video>;
+  },
 };
 
 const mdPlugins: PluggableList = [
-    [remarkMermaid],
-    [emoji, {accessible: true, emoticon: false}],
-    [remarkUnwrapImages],
-    [remarkCode],
-    [remarkGfm]
+  [remarkMermaid],
+  [emoji, {accessible: true, emoticon: false}],
+  [remarkUnwrapImages],
+  [remarkCode],
+  [remarkGfm]
 ];
 const rhPlugins: PluggableList = [
-    [rehypeRaw, {passThrough: ['mdxJsxTextElement', 'mdxJsxFlowElement'], allowDangerousHtml: true}],
+  [rehypeRaw, {passThrough: ['mdxJsxTextElement', 'mdxJsxFlowElement'], allowDangerousHtml: true}],
 ];
 
 const options: MDXRemoteProps["options"] = {
-    parseFrontmatter: true,
-    mdxOptions: {
-        rehypePlugins: rhPlugins,
-        remarkPlugins: mdPlugins,
-    }
+  parseFrontmatter: true,
+  mdxOptions: {
+    rehypePlugins: rhPlugins,
+    remarkPlugins: mdPlugins,
+  }
 }
 
 export const getMdxDataByPath = async ({path}: { path: string }) => {
-    const source = await read(path);
-    return compileMDX({source, options, components: cmp});
+  const source = await read(path);
+  return compileMDX({source, options, components: cmp});
 }
