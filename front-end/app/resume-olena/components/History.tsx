6014958e6b287c8603f36806c7cb6841d724db65
Oleksii Popov <alexejpopovua@gmail.com>
2025-03-04 22:13:09 +0100

    refactor(front-end): migrate Olena's cv to the latest view

--- front-end/app/resume-olena/components/History.tsx
+++ front-end/app/resume-olena/components/History.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import moment, {Duration} from "moment";
-import clsx from "clsx";
+import React from 'react';
+import moment from 'moment';
+import clsx from 'clsx';
 
-import CVSection from "./CVSection";
-import data from "../data/data";
-import Tag from "@/components/primitive/Tag";
+import CVSection from './CVSection';
+import data from '../data/data';
+import Tag from '@/components/primitive/Tag';
 
 type Props = {
     experience: typeof data.experience;
@@ -13,18 +13,9 @@ type Props = {
 /**
  * Converts "moment" module duration object into the humanized string
  */
-const getHumanizedDuration = (duration: moment.Duration) => {
-    let years = duration.years();
-    let months = duration.months();
-    const days = duration.days();
-
-    //correction takes days into account
-    if (days / 31 > 0.75) {
-        duration.add(1, "month");
-        duration.subtract(days, "day");
-        months = duration.months();
-        years = duration.years();
-    }
+function getHumanizedDuration(duration: moment.Duration) {
+    const years = duration.years();
+    const months = duration.months();
 
     //formatted output
     const result = [];
@@ -34,42 +25,81 @@ const getHumanizedDuration = (duration: moment.Duration) => {
     if (months > 0) {
         result.push(months === 1 ? `${months} month` : `${months} months`);
     }
-    return result.join(" ");
+    return result.join(' ');
 }
 
-const History = (props: Props) => {
-    const historyDurations = props.experience.map((item) =>
-        moment.duration(
-            (item.dateEnd ? moment(item.dateEnd, "MMM YYYY", "en") : moment()).diff(
-                moment(item.dateStart, "MMM YYYY", "en")
-            )
-        )
-    );
+function Description(props: { description: string | string[]; className?: string }) {
+    if (Array.isArray(props.description)) {
+        return (
+            <div className={clsx('flex flex-col gap-2', props.className)}>
+                {props.description.map((descr, i) => (
+                    <div
+                        key={descr.slice(0, 20)}
+                        className={clsx('flex flex-col gap-2', {
+                            'border-b': i !== props.description.length - 1,
+                        })}
+                    >
+                        {descr}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
-    const totalDuration = historyDurations.reduce((acc, val) => acc.add(val), moment.duration());
-    const historyDurationValues = historyDurations.map(getHumanizedDuration);
+    return <div className="description col-span-3 col-start-2 pb-2">{props.description}</div>;
+}
+
+function Stack(props: { stack: string }) {
+    return (
+        <div className="description flex flex-row flex-wrap gap-2">
+            {props.stack.split(', ').map((item) => (
+                <Tag key={item} item={item} />
+            ))}
+        </div>
+    );
+}
+
+export default function History(props: Props) {
+    const historyDurations = props.experience.map((item) =>
+        moment.duration((item.dateEnd ? moment(item.dateEnd, 'MMM YYYY', 'en') : moment()).diff(moment(item.dateStart, 'MMM YYYY', 'en'))),
+    );
+    const historyDurationValues = historyDurations.map((duration) => getHumanizedDuration(duration));
+
+    let totalDuration = moment.duration();
+    for (let i = 0; i < historyDurations.length; i++) {
+        totalDuration = totalDuration.add(historyDurations[i]);
+    }
 
     return (
         <CVSection cls="history" title={`Work history (${getHumanizedDuration(totalDuration)})`}>
             {props.experience.map((item, index) => (
-                <section key={`${item.company}-${item.title}`} className="history-section flex flex-col gap-1">
-                    <h3 className="title">{item.title}</h3>
-                    <div className="company-name font-bold">{item.company}</div>
-                    <div
-                        className="font-bold">{`${item.dateStart} - ${item.dateEnd ? item.dateEnd : "Now"} (${historyDurationValues[index]})`}</div>
-                    {item.stack && <div
-                        className="description flex flex-row flex-wrap gap-1">{item.stack.split(", ").map(item =>
-                        <Tag key={item} item={item}/>)}</div>}
-                    {Array.isArray(item.description) ? item.description.map((descr, i) => <div
-                            key={descr.slice(0, 20)}
-                            className={clsx("flex flex-col gap-2", {
-                                "border-b": i !== item.description.length - 1
-                            })}>{descr}</div>) :
-                        <div className="description col-start-2 col-span-3 pb-2">{item.description}</div>}
+                <section key={`${item.company}-${item.company}`} className="history-section flex flex-col gap-2">
+                    <h3 className="company-name font-bold">
+                        {item.website ? (
+                            <a className="no-underline" href={item.website}>
+                                {item.company}
+                            </a>
+                        ) : (
+                            item.company
+                        )}
+                    </h3>
+                    <div className="font-bold">{`${item.dateStart} - ${item.dateEnd ? item.dateEnd : 'Now'} (${historyDurationValues[index]})`}</div>
+                    <Description description={item.description} className="italic" />
+                    {item.website ? (
+                        <a href={item.website} className="mb-2 text-sm italic">
+                            {item.website}
+                        </a>
+                    ) : null}
+                    {item.stack ? <Stack stack={item.stack} /> : null}
+                    {item.positions &&
+                        item.positions.map((position) => (
+                            <div key={position.title} className="project">
+                                <h4 className="title">&gt; {position.title}</h4>
+                                <Description description={position.description} />
+                            </div>
+                        ))}
                 </section>
             ))}
         </CVSection>
     );
 }
-
-export default History;
--- front-end/app/resume-olena/data/data.ts
+++ front-end/app/resume-olena/data/data.ts
@@ -1,4 +1,4 @@
-const data = {
+const data = Object.seal({
     user: {
         name: "Olena",
         surname: "Tymchenko",
@@ -6,7 +6,8 @@ const data = {
         position: "Marketing Manager",
         Email: "alenatymchenko@gmail.com",
         Address: "Klagenfurt am Wörthersee, Austria",
-        Phone: "+43 676 540 84 32"
+        Phone: "+43 676 540 84 32",
+        EducationLevel: "MSc"
     },
     intro: {
         title: "Intro",
@@ -16,65 +17,104 @@ const data = {
     },
     contacts: {
         Nationality: "Dutch and Ukrainian",
-        "I speak": "English, German (A2), Dutch, Ukrainian, Russian"
+        "I speak": "English, German (B1), Dutch, Ukrainian, Russian"
     },
     skills: [
         {
-            "name": "SEO",
-            "description": "Analysis of website structure, optimization of web pages, copyrighting, communication with website visitors, internet marketing, user analysis, reporting"
+            name: "SEO",
+            description: "Analysis of website structure, optimization of web pages, copyrighting, communication with website visitors, internet marketing, user analysis, reporting"
         },
         {
-            "name": "Banking experience",
-            "description": "Performed data analysis in money transfer reporting. Worked with transfer systems (CONTACT, Western Union, UNIStream, Lider, Anelik, MIGOM, SWIFT). Supported individuals and businesses (bank transfer, account opening, money management, mortgage, deposits, and loans). Administrative work"
+            name: "Banking experience",
+            description: "Performed data analysis in money transfer reporting. Worked with transfer systems (CONTACT, Western Union, UNIStream, Lider, Anelik, MIGOM, SWIFT). Supported individuals and businesses (bank transfer, account opening, money management, mortgage, deposits, and loans). Administrative work"
         }
     ],
     certificates: [],
     experience: [
         {
-            "company": "Ukrainian hosting (Ukraine)",
-            "title": "SEO optimizer",
-            "dateStart": "April 2010",
-            "dateEnd": "May 2016",
-            "description": "Web page optimization for better positioning in Google and Yandex",
-            "stack": ""
+            company: "Ukrainian hosting (Kharkiv, Ukraine) 🇺🇦",
+            dateStart: "April 2010",
+            dateEnd: "May 2016",
+            positions: [
+                {
+                    title: "SEO optimizer",
+                    dateStart: "April 2010",
+                    dateEnd: "May 2016",
+                    description: [
+                        "Web page optimization for better positioning in Google and Yandex"
+                    ]
+                }
+            ],
+            description: "Hosting and IT services company",
+            stack: ""
         },
         {
-            "company": "FC Bank (Ukraine)",
-            "title": "Economist",
-            "dateStart": "December 2009",
-            "dateEnd": "March 2010",
-            "description": "Writing reports, working with databases, cash flow control",
-            "stack": ""
+            company: "FC Bank (Kharkiv, Ukraine) 🇺🇦",
+            website: undefined,
+            dateStart: "December 2009",
+            dateEnd: "March 2010",
+            positions: [
+                {
+                    title: "Economist",
+                    dateStart: "December 2009",
+                    dateEnd: "March 2010",
+                    description: [
+                        "Writing reports, working with databases, cash flow control"
+                    ]
+                }
+            ],
+            description: "Commercial bank",
+            stack: ""
         },
         {
-            "company": "ProCredit Bank (Ukraine)",
-            "title": "Customer service representative",
-            "dateStart": "August 2008",
-            "dateEnd": "August 2009",
-            "description": "Writing reports, working with customers, account management, deposit portfolio, loan portfolio, working with plastic cards",
-            "stack": ""
+            company: "ProCredit Bank (Kharkiv, Ukraine) 🇺🇦",
+            website: undefined,
+            dateStart: "August 2008",
+            dateEnd: "August 2009",
+            positions: [
+                {
+                    title: "Customer service representative",
+                    dateStart: "August 2008",
+                    dateEnd: "August 2009",
+                    description: [
+                        "Writing reports, working with customers, account management, deposit portfolio, loan portfolio, working with plastic cards"
+                    ]
+                }
+            ],
+            description: "Commercial bank",
+            stack: ""
         },
         {
-            "company": "TNS-Ukraine (Ukraine)",
-            "title": "Operator interviewer",
-            "dateStart": "2004",
-            "dateEnd": "2008",
-            "description": "Collection and primary processing of data for further sociological research",
-            "stack": ""
+            company: "TNS-Ukraine (Kharkiv, Ukraine) 🇺🇦",
+            website: undefined,
+            dateStart: "2004",
+            dateEnd: "2008",
+            positions: [
+                {
+                    title: "Operator interviewer",
+                    dateStart: "2004",
+                    dateEnd: "2008",
+                    description: [
+                        "Collection and primary processing of data for further sociological research"
+                    ]
+                }
+            ],
+            description: "Marketing agency",
+            stack: ""
         }
     ],
     education: [
         {
-            "company": "Kharkiv University of Banking (Ukraine)",
-            "title": "Master of Science (MSc)",
-            "date": "2007 - 2008",
-            "description": "Banking"
+            company: "Kharkiv University of Banking (Ukraine)",
+            title: "Master of Science (MSc)",
+            date: "2007 - 2008",
+            description: "Banking"
         },
         {
-            "company": "Kharkiv University of Banking (Ukraine)",
-            "title": "Bachelor of Science (BSc)",
-            "date": "2003 - 2007",
-            "description": "Banking"
+            company: "Kharkiv University of Banking (Ukraine)",
+            title: "Bachelor of Science (BSc)",
+            date: "2003 - 2007",
+            description: "Banking"
         }
     ],
     ownProjects: [
@@ -91,6 +131,6 @@ const data = {
             ]
         }
     ]
-};
+});
 
 export default data;
