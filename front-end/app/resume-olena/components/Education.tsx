f8fffce1636cb304c94e0183711663dc1ef9b163
Oleksii Popov <alexejpopovua@gmail.com>
2025-03-04 22:13:09 +0100

    refactor(front-end): migrate Olena's cv to the latest view

--- front-end/app/resume-olena/components/Education.tsx
+++ front-end/app/resume-olena/components/Education.tsx
@@ -12,8 +12,8 @@ const Education = (props: Props) => (
         {props.education.map((item) => (
             <Fragment key={item.title}>
                 <h3 className="title">{item.title}</h3>
-                <div className="item-key pr-4">{item.date}</div>
-                <div className="company-name italic">{item.company}</div>
+                <div className="item-key pr-4 mb-2">{item.date}</div>
+                <div className="company-name mb-2 italic">{item.company}</div>
                 <div className="description">{item.description}</div>
             </Fragment>
         ))}
--- front-end/app/resume-olena/page.tsx
+++ front-end/app/resume-olena/page.tsx
@@ -1,51 +1,50 @@
-import React, {useMemo} from "react";
+import React from "react";
+import {Metadata} from "next";
 
-import {getPagePathByDirName, readFrontMatterWithContent} from "@/lib/files";
+import {getPagePathByDirName} from "@/lib/files";
 import {PageSchema} from "@/content/pages/resume/schema";
 import Header from "@/app/resume-olena/components/Header";
-import Intro from "@/app/resume-olena/components/Intro";
 import Skills from "@/app/resume-olena/components/Skills";
 import History from "@/app/resume-olena/components/History";
-import OwnProjects from "@/app/resume-olena/components/OwnProjects";
 import Education from "@/app/resume-olena/components/Education";
-import data from "@/app/resume-olena/data/data";
+import renderData from "@/app/resume-olena/data/data";
 import {content} from "@/app/configuration/content";
+import {environment} from "@/app/configuration/environment";
+import {ensurePathSlash} from "@/lib/utils";
+import {getOGImageURL} from "@/lib/image";
+import {getFrontMatterDataByPath} from "@/lib/mdx-utils";
 
 const pageSlug = "resume-olena";
 
-async function getPost({slug}: { slug: string }) {
-    const {frontMatter, content} = await readFrontMatterWithContent<PageSchema>(getPagePathByDirName(pageSlug));
+export async function generateMetadata(): Promise<Metadata> {
+    const frontMatter = await getFrontMatterDataByPath<PageSchema>(getPagePathByDirName(pageSlug));
+    const ogImage = getOGImageURL({src: frontMatter.thumbnail});
 
     return {
-        frontMatter,
-        slug,
-        content
-    }
-}
-
-export async function generateMetadata({params}: any) {
-    const page = await getPost(params);
-
-    return {
-        title: `${page.frontMatter.title} - ${content.authorName}`,
-        description: page.frontMatter.description,
-        keywords: page.frontMatter.keywords,
-        openGraph: {
-            title: `${page.frontMatter.title} - ${content.authorName}`,
-            description: page.frontMatter.description
+        title: `${frontMatter.title} - ${content.authorName}`,
+        description: frontMatter.description,
+        metadataBase: new URL(environment.url),
+        alternates: {
+            canonical: ensurePathSlash(`/${pageSlug}`)
         },
+        keywords: frontMatter.keywords,
+        openGraph: {
+            title: `${frontMatter.title} - ${content.authorName}`,
+            description: frontMatter.description,
+            images: [
+                ogImage
+            ]
+        }
     }
 }
 
 export default async function Post() {
-    const renderData = useMemo(() => data, []);
-
     return (
-        <article className='prose prose-sm md:prose-base lg:prose-lg prose-pre:bg-white prose-pre:p-0 mx-auto p-4 print:p-0 print:pt-8'>
+        <article
+            className='prose prose-sm md:prose-base lg:prose-lg prose-pre:bg-white prose-pre:p-0 mx-auto p-4 print:p-0 print:pt-2'>
             <Header user={renderData.user} contacts={renderData.contacts}/>
             <Skills skills={renderData.skills}/>
             <History experience={renderData.experience}/>
-              {/*<OwnProjects projects={renderData.ownProjects}/>*/}
             <Education education={renderData.education}/>
         </article>
     );
