5160681d38bde1c27b236ffa55b6bef085575d4f
Oleksii Popov <alexejpopovua@gmail.com>
2025-03-04 22:13:09 +0100

    refactor(front-end): migrate Olena's cv to the latest view

--- front-end/app/resume-olena/components/Header.tsx
+++ front-end/app/resume-olena/components/Header.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
 import Image from 'next/image';
 
-import data from "../data/data";
-import {getInsideImageURL} from "@/lib/image";
+import data from '../data/data';
+import { getInsideImageURL } from '@/lib/image';
 
 type Props = {
-  user: typeof data.user;
-  contacts: typeof data.contacts;
-}
+    user: typeof data.user;
+    contacts: typeof data.contacts;
+};
 
 export default async function Header(props: Props) {
-  const contacts = [props.user.Address, props.user.Email, props.user.Phone, `I speak ${props.contacts["I speak"]}`, `Nationality: ${props.contacts.Nationality}`];
-  const insideUrlProps: getInsideImageURL.Props = { width: 500, height: 400, src: "/pages/resume-olena/resume-olena-profile.jpg" };
-  const insideUrl = getInsideImageURL(insideUrlProps);
+    const insideUrlProps: getInsideImageURL.Props = {
+        width: 500,
+        height: 400,
+        src: '/pages/resume-olena/resume-olena-profile.jpg',
+    };
+    const insideUrl = getInsideImageURL(insideUrlProps);
 
-  return (
-    <div className="flex flex-col items-center">
-      <h1>{props.user.name} {props.user.surname}</h1>
+    return (
+        <div className="flex flex-col items-center">
+            <h1>
+                {props.user.name} {props.user.surname}
+            </h1>
 
-      <div className="flex flex-row gap-8 w-full">
-        <Image
-          className="flex-0 w-36 not-prose object-contain object-top"
-          src={insideUrl}
-          alt="Profile photo of Olena Tymchenko"
-          unoptimized={true}
-          width={insideUrlProps.width}
-          height={insideUrlProps.height}
-        />
-        <div className="flex-4 text-left mb-4 sm:mb-0 mr-8">
-          <div className="text-3xl mb-4">{props.user.position}</div>
-          {contacts.map(contact => (
-            <div key={contact}>{contact}</div>
-          ))}
+            <div className="flex w-full flex-row gap-8">
+                <Image
+                    className="flex-0 not-prose w-36 object-contain object-top"
+                    src={insideUrl}
+                    alt="Profile photo of Olena Tymchenko"
+                    unoptimized={true}
+                    width={insideUrlProps.width}
+                    height={insideUrlProps.height}
+                />
+                <div className="flex-4 mb-4 mr-8 text-left sm:mb-0">
+                    <div className="mb-4 text-3xl">{props.user.position}</div>
+                    <div className="max-w-sm whitespace-nowrap">🇦🇹&nbsp;{props.user.Address}</div>
+                    <div className="max-w-sm whitespace-nowrap">
+                        📨&nbsp;<a href={`mailto:${props.user.Email}`}>{props.user.Email}</a>
+                    </div>
+                    <div className="max-w-sm whitespace-nowrap">
+                        &#128222;&nbsp;<a href={`callto:${props.user.Phone}`}>{props.user.Phone}</a>
+                    </div>
+                    <div className="max-w-sm whitespace-nowrap">🌍&nbsp;I speak {props.contacts['I speak']}</div>
+                    <div className="max-w-sm whitespace-nowrap">🇳🇱&nbsp;🇺🇦&nbsp;Nationality: {props.contacts.Nationality}</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
