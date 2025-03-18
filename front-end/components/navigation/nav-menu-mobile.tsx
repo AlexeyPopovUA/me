6ca88c1763ece12477d5aa9b9b84e142d877d10b
Oleksii Popov <alexejpopovua@gmail.com>
2025-03-18 21:00:30 +0100

    chore(front-end): update dependencies and improve Tailwind CSS configuration

--- front-end/components/navigation/nav-menu-mobile.tsx
+++ front-end/components/navigation/nav-menu-mobile.tsx
@@ -5,7 +5,7 @@ import {clsx} from "clsx";
 import Link from "next/link";
 
 const itemCls = "select-none hover:text-amber-500 duration-200";
-const titleItemCls = "select-none rounded-md leading-none no-underline outline-none text-2xl";
+const titleItemCls = "select-none rounded-md leading-none no-underline outline-hidden text-2xl";
 
 const NavMenuMobile = ({items}: { items: { title: string; link: string; }[] }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,7 +32,7 @@ const NavMenuMobile = ({items}: { items: { title: string; link: string; }[] }) =
             </div>
             <div className="flex flex-row grow justify-end text-lg">
                 <button data-collapse-toggle="navbar-default" type="button" onClick={handleMenuClick}
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
