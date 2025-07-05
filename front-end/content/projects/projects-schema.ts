73e20a8c1f8d50a0ca8358a32854a48d61a165f9
Oleksii Popov <alexejpopovua@gmail.com>
2025-07-05 16:03:03 +0200

    feat: add technologies section to article.mdx files for improved project documentation

--- front-end/content/projects/projects-schema.ts
+++ front-end/content/projects/projects-schema.ts
@@ -4,6 +4,7 @@ export interface ProjectsSchema extends CommonPostSchema {
     company?: string;
     type: "Employment" | "Freelance" | "Own project";
     gallery: string[];
+    technologies: string[];
     URL?: string;
     state?: string;
 }
